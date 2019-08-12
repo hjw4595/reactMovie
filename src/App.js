765ebdae1102c7d1ba67208031ee0c7f0fbeca61
hjw4595 <hongjung73@gmail.com>
2019-08-12 23:54:25 +0900

    dynamic Components(map())

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from 'prop-types';
 
+const lists = [
+  {
+    id: "1",
+    name: "일",
+    rating: 5,
+  },
+  {
+    id: "2",
+    name: "이",
+    rating: 4,
+  },
+  {
+    id: "3",
+    name: "삼",
+    rating: 4,
+  }
+  , {
+    id: "4",
+    name: "사",
+    rating: 5,
+  },
+  {
+    id: "5",
+    name: "오",
+    rating: 3,
+  }
+];
+
+function RenderLists({name, rating}){
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>{name} is  {rating}</h2> 
+    </div>
+  );
+}
+RenderLists.propTypes = {
+  name: PropTypes.string.isRequired,
+  rating: PropTypes.number
+};
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {lists.map(list => (
+        <RenderLists
+          key={list.id}
+          name={list.name}
+          rating={list.rating}
+        />
+      ))}
     </div>
   );
 }
