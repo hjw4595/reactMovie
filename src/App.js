25ca34b6c047ae29f86c1364b3037341d7c74379
hjw4595 <hongjung73@gmail.com>
2019-08-13 10:09:55 +0900

    lifecycle

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    console.log("mount")
+  }
   state = {
     count: 0
   };
@@ -10,7 +14,14 @@ class App extends React.Component {
   minus = () => {
     this.setState(Arithmetic => ({count: Arithmetic.count - 1}));
   };
+  componentDidMount(){
+    console.log("rendered")
+  }
+  componentDidUpdate(){
+    console.log("just update")
+  }
   render() {
+    console.log("render")
     return (
       <div>
         <h1>The number is: {this.state.count}</h1>
