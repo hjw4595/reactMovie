7ece5f0a725adab688690a116f5cdb43a73aa614
hjw4595 <hongjung73@gmail.com>
2019-08-14 10:17:15 +0900

    css

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
 import React from "react";
-import axios from "axios"
+import axios from "axios";
 import Movie from "./Movie";
+import "./App.css";
+
 class App extends React.Component {
-  state ={
+  state = {
     isLoading: true,
-    movies : []
+    movies: []
   };
   getMovies = async () => {
     const {
       data: {
-        data : {movies }
+        data: { movies }
       }
     } = await axios.get(
-      "https://yts.lt/api/v2/list_movies.json?sort_by=rating "
-      );
-    this.setState({movies, isLoading: false})
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
   };
   componentDidMount() {
     this.getMovies();
   }
-
   render() {
     const { isLoading, movies } = this.state;
     return (
-      <div>
-        {isLoading
-          ? "Loading..."
-          : movies.map(movie => (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movie => (
               <Movie
                 key={movie.id}
                 id={movie.id}
@@ -34,9 +39,12 @@ class App extends React.Component {
                 title={movie.title}
                 summary={movie.summary}
                 poster={movie.medium_cover_image}
+                genres={movie.genres}
               />
             ))}
-      </div>
+          </div>
+        )}
+      </section>
     );
   }
 }
